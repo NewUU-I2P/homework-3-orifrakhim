575a4a3ec797db8217a7325f615c6f37d5210563
orifrakhim <147371787+orifrakhim@users.noreply.github.com>
2023-11-14 21:45:59 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,7 +3,16 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
-
+    std::string result;
+    if (macAddress[0] == 'F' and macAddress[1] == 'F') {
+        result = "Broadcast";
+    } else if ((int)macAddress[1] % 2 == 0) {
+        result = "Unicast";
+    } else if ((int)macAddress[1] % 2 == 1) {
+        result = "Multicast";
+    }
+    
+    return result;    
     // make use of control flow statements
     // return result;
 }
