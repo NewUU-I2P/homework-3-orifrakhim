b222735375669d55b56aaedbca78a136a37fa4cb
orifrakhim <147371787+orifrakhim@users.noreply.github.com>
2023-11-14 21:43:58 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,7 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
+    i > j && i > k ? result = i : j > i && j > k ? result = j : result = k;
 
     return result;
 }
