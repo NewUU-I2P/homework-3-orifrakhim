1eff8a3ae75244dc4cc46f19dd6507c33f83d054
orifrakhim <147371787+orifrakhim@users.noreply.github.com>
2023-11-14 21:43:06 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,16 @@
-void problemSolution1(float consumed_water) {
+float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
+
+    if (consumed_water <= 30.0) {
+        cost = 13 + consumed_water * 0.4;
+    } else if (consumed_water <= 50.0) {
+        cost = 13 + 30.0 * 0.4 + (consumed_water - 30.0) * 0.12;
+    } else if (consumed_water <= 60.0) {
+        cost = 13 + 30.0 * 0.4 + 20.0 * 0.12 + (consumed_water - 50.0) * 1.4;
+    } else {
+        cost = 13 + 30.0 * 0.4 + 20.0 * 0.12 + 10.0 * 1.4 + (consumed_water - 60.0) * 1.5;
+    }
 
     return cost;
 }
